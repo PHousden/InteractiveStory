cc643f8b06a75f3f7f17f23aeb5dd14fd886f4bc
Philip Housden <94585775+PHousden@users.noreply.github.com>
2021-11-18 11:19:41 +0700

    Interactive App

---
+++ app/src/main/java/com/philip/interactivestory/MainActivity.java
@@ -0,0 +1,43 @@
+package com.philip.interactivestory;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText) findViewById(R.id.editTextTextPersonName);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+
+    }
+}
